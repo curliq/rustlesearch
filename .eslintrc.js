963acd0347c2251b40b1ebbe26a28041b1870790
alg1142 <48770555+alg1142@users.noreply.github.com>
2019-06-06 23:47:03 +0300

    add linting and relint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: ['standard'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  rules: {
+    'space-before-function-paren': ['error', 'never'],
+    'object-curly-spacing': ['error', 'never'],
+    'operator-linebreak': ['error', 'before'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'curly': ['error', 'multi-or-nest'],
+  },
+}
--- indexToElasticsearch.js
+++ indexToElasticsearch.js
@@ -1,58 +1,58 @@
 const fs = require('fs')
 const path = require('path')
 const glob = require('glob')
-const { Client } = require('@elastic/elasticsearch')
+const {Client} = require('@elastic/elasticsearch')
 const _ = require('lodash')
 const util = require('util')
 const etl = require('etl')
 
-const base_path = './data/rustle'
-const message_regex = /^\[(.*?)\]\s(.*?):\s(.*)$/
-const client = new Client({ node: 'http://localhost:9200' })
+const basePath = './data/rustle'
+const messageRegex = /^\[(.*?)\]\s(.*?):\s(.*)$/
+const client = new Client({node: 'http://localhost:9200'})
 const writeFile = util.promisify(fs.writeFile)
 
 const lineToMessage = (line, channel) => {
   try {
-    const replaced_line = line.replace('\r', '')
-    const matched = replaced_line.match(message_regex)
-    const [__, ts_str, username, text] = matched
-    const ts = new Date(ts_str)
+    const replacedLine = line.replace('\r', '')
+    const matched = replacedLine.match(messageRegex)
+    const [, tsStr, username, text] = matched
+    const ts = new Date(tsStr)
     return {
       ts: ts.toISOString(),
       channel,
       username,
-      text
+      text,
     }
   } catch (e) {
-    // console.log(e)
+    console.error(e)
   }
 }
-const paths_to_messages = async paths => {
-  for (let file_paths of _.chunk(paths, 50)) {
-    for (let file_path of file_paths) {
-      const channel = path.parse(file_path).name.split('::')[0]
+
+const pathsToMessages = async paths => {
+  for (let filePaths of _.chunk(paths, 50)) {
+    for (let filePath of filePaths) {
+      const channel = path.parse(filePath).name.split('::')[0]
 
       await etl
-        .file(file_path)
+        .file(filePath)
         .pipe(etl.split())
         .pipe(etl.map(line => lineToMessage(line.text, channel)))
         .pipe(etl.collect(2000))
         .pipe(
           etl.elastic.index(client, 'oversearch', 'message', {
-            concurrency: 10
+            concurrency: 10,
           })
         )
         .promise()
     }
-    await writeFile('./cache.txt', file_paths.join('\n') + '\n', {
+    await writeFile('./cache.txt', filePaths.join('\n') + '\n', {
       encoding: 'utf8',
-      flag: 'a'
+      flag: 'a',
     })
   }
 }
 
-const all_paths = glob.sync(`${base_path}/*.txt`)
-const cache = fs.readFileSync('./cache.txt', { encoding: 'utf8' }).split('\n')
-const paths = all_paths.filter(x => !cache.includes(x))
-paths_to_messages(paths)
-// paths_to_messages(paths)
+const allPaths = glob.sync(`${basePath}/*.txt`)
+const cache = fs.readFileSync('./cache.txt', {encoding: 'utf8'}).split('\n')
+const paths = allPaths.filter(x => !cache.includes(x))
+pathsToMessages(paths)
