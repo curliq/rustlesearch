7c93c26e0bcb577c24e3c5ef056bbcaa4ee9ffe7
johnpyp <johnpaulpenaloza@gmail.com>
2019-06-05 23:53:21 -0400

    Even more gooder concurrency for ingesting food

--- to_elasticsearch.js
+++ to_elasticsearch.js
@@ -6,7 +6,9 @@ const _ = require('lodash')
 const base_path = './data/rustle'
 const message_regex = /^\[(.*?)\]\s(.*?):\s(.*)$/
 const client = new Client({ node: 'http://localhost:9200' })
+const util = require('util')
 
+const readFile = util.promisify(fs.readFile)
 let success_count = 0
 let count = 0
 
@@ -48,7 +50,7 @@ const contents_to_messages = (contents, channel) => {
   return messages
 }
 const bulk_to_es = async messages => {
-  for (let mchunk of _.chunk(messages, 4000)) {
+  for (let mchunk of _.chunk(messages, 3000)) {
     const formatted_messages = messages_to_bulk_array(mchunk)
 
     const { body: bulkResponses } = await client.bulk({
@@ -59,14 +61,25 @@ const bulk_to_es = async messages => {
     }
   }
 }
+const contents_to_es = async ({ contents, file_path }) => {
+  const channel = path.parse(file_path).name.split('::')[0]
+  const messages = contents_to_messages(contents, channel)
+  await bulk_to_es(messages)
+}
 const paths_to_messages = async paths => {
-  for (let file_path of paths) {
-    const contents = fs
-      .readFileSync(file_path, { encoding: 'utf8' })
-      .split('\n')
-    const channel = path.parse(file_path).name.split('::')[0]
-    const messages = contents_to_messages(contents, channel)
-    await bulk_to_es(messages)
+  for (let file_paths of _.chunk(paths, 50)) {
+    const contents20 = await Promise.all(
+      file_paths.map(async x => {
+        let contents = await readFile(x, 'utf8')
+        return {
+          contents: contents.split('\n'),
+          file_path: x
+        }
+      })
+    )
+    for (let contents3 of _.chunk(contents20, 5)) {
+      await Promise.all(contents3.map(contents_to_es))
+    }
   }
 }
 
