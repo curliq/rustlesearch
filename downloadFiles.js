dd022cf13d0855bb6253914342f7195d5e7c236c
johnpyp <johnpaulpenaloza@gmail.com>
2019-06-08 20:50:03 -0400

    Restructered download script with ramda

--- downloadFiles.js
+++ downloadFiles.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
 const request = require('request')
-const _ = require('lodash')
 const util = require('util')
-const glob = require('glob')
+const fg = require('fast-glob')
 const {subDays, format} = require('date-fns')
+const Promise = require('bluebird')
+const R = require('ramda')
 
 const rp = util.promisify(request)
 const readFile = util.promisify(fs.readFile)
@@ -12,24 +13,40 @@ const writeFile = util.promisify(fs.writeFile)
 const baseUrl = 'https://overrustlelogs.net'
 const basePath = './data/rustle'
 
-function getUrlList(channels, daysBack, farthestYears) {
-  let urls = []
-  for (let dayBack of _.range(1, daysBack)) {
-    let d = subDays(new Date(), dayBack)
-    let fullFormat = format(d, 'MMMM YYYY/YYYY-MM-DD')
-    let dateFormat = format(d, 'YYYY-MM-DD')
-    let yearFormat = format(d, 'YYYY')
-    for (let channel of channels) {
-      if (parseInt(yearFormat) >= farthestYears[channel]) {
-        let path = `${basePath}/${channel}::${dateFormat}.txt`
-        let url = `${baseUrl}/${channel} chatlog/${fullFormat}.txt`
-        urls.push([path, url])
-      }
-    }
-  }
-  return urls
+const fullDateFormat = date => format(date, 'MMMM YYYY/YYYY-MM-DD')
+const fileDateFormat = date => format(date, 'YYYY-MM-DD')
+const yearDateFormat = date => parseInt(format(date, 'YYYY'))
+
+const toPathAndUrl = ({channel, fileDate, fullDate}) => {
+  return [
+    `${basePath}/${channel}::${fileDate}.txt`,
+    `${baseUrl}/${channel} chatlog/${fullDate}.txt`,
+  ]
 }
-async function downloadFile([path, url], json = false) {
+const getChannelMonths = async channel => {
+  const {body: contents} = await rp({
+    uri: `${baseUrl}/api/v1/${channel}/months.json`,
+    json: true,
+  })
+  return [channel, parseInt(contents[contents.length - 1].slice(-4))]
+}
+const getUrlList = (channels, daysBack, farthestYears) =>
+  R.pipe(
+    R.map(dayBack => subDays(new Date(), dayBack)),
+    R.map(d =>
+      [fullDateFormat, fileDateFormat, yearDateFormat].map(fn => fn(d))
+    ),
+    R.map(([fullDate, fileDate, yearDate]) => {
+      const isValidDate = channel => yearDate >= farthestYears[channel]
+      const validChannels = channels.filter(channel => isValidDate(channel))
+      return validChannels.map(channel =>
+        toPathAndUrl({channel, fileDate, fullDate})
+      )
+    }),
+    R.unnest
+  )(R.range(1, daysBack))
+
+const downloadFile = async([path, url], json = false) => {
   const res = await rp({uri: url, json})
   if (res.statusCode === 200) {
     await writeFile(path, res.body)
@@ -37,37 +54,29 @@ async function downloadFile([path, url], json = false) {
     return path
   } else return false
 }
-async function getFarthestYears(channels) {
-  const farthestYears = {}
-  for (let channel of channels) {
-    let {body: contents} = await rp({
-      uri: `${baseUrl}/api/v1/${channel}/months.json`,
-      json: true,
-    })
-    farthestYears[channel] = parseInt(contents[contents.length - 1].slice(-4))
-  }
-  return farthestYears
+
+const getFarthestYears = async channels => {
+  const farthestYears = await Promise.map(channels, getChannelMonths, {
+    concurrency: 20,
+  })
+  return R.fromPairs(farthestYears)
 }
 
-async function main() {
+const main = async() => {
   const channelsFile = await readFile('./channels.txt')
   const channels = channelsFile
     .toString()
     .split('\n')
     .filter(x => x !== '')
-  console.log(channels)
 
   const farthestYears = await getFarthestYears(channels)
   console.log(farthestYears)
-
   const allUrls = getUrlList(channels, process.argv[2] || 10, farthestYears)
-  const urlsDownloaded = glob.sync(`${basePath}/*.txt`)
+  const urlsDownloaded = await fg.async(`${basePath}/*.txt`)
   const urls = allUrls.filter(x => !urlsDownloaded.includes(x[0]))
 
   console.log(allUrls.length, urlsDownloaded.length, urls.length)
-  for (let chunk of _.chunk(urls, 20)) {
-    await Promise.all(chunk.map(downloadFile))
-  }
+  Promise.map(urls, downloadFile, {concurrency: 20})
 }
 
 main()
