f906bc52671f71b06b5cebbbec150569bfc3dc1f
alg1142 <48770555+alg1142@users.noreply.github.com>
2019-06-10 21:58:53 +0300

    new logger poggers

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,27 +1,15 @@
-const winston = require('winston')
-const expressWinston = require('express-winston')
+const pino = require('pino')
+const {isProd} = require('@lib/environment')
 
-const levels = {
-  ...winston.config.syslog.levels,
-  warn: 4,
+const name = process.env.APP_NAME
+
+const pinoOptions = {
+  name,
+  base: {name},
+  level: process.env.LOG_LEVEL,
+  prettyPrint: !isProd(),
 }
 
-const winstonConfig = {
-  levels,
-  transports: [new winston.transports.Console({level: process.env.LOG_LEVEL})],
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
-}
+const logger = pino(pinoOptions)
 
-const expressWinstonConfig = {
-  ...winstonConfig,
-  msg: '{{req.method}} {{req.url}} {{res.statusCode}} - {{res.responseTime}}ms',
-  meta: false,
-}
-
-const logger = winston.createLogger(winstonConfig)
-const expressLogger = expressWinston.logger(expressWinstonConfig)
-
-module.exports = {expressLogger, logger}
+module.exports = {logger}
