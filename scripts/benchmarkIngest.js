d2828ebfdf845c40bf9d7a30d3ddd235a40d2f7e
johnpyp <johnpaulpenaloza@gmail.com>
2019-06-09 22:15:41 -0400

    Added ingest speed benchmark script

---
+++ scripts/benchmarkIngest.js
@@ -0,0 +1,24 @@
+const {Client} = require('@elastic/elasticsearch')
+
+const client = new Client({node: process.env.ELASTIC_LOCATION})
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+const average = arr => arr.reduce((a, b) => a + b, 0) / arr.length
+
+const getCount = async() => {
+  const result = await client.count({index: process.env.INDEX_NAME})
+  return result.body.count
+}
+
+const main = async() => {
+  const res1 = await getCount()
+  console.log('1st:', res1)
+  await sleep(1000 * 60)
+  const res2 = await getCount()
+  console.log('2nd:', res2)
+  await sleep(1000 * 60)
+  const res3 = await getCount()
+  console.log('3rd:', res3)
+  console.log(average([res2 - res1, res3 - res2]))
+}
+
+main()
