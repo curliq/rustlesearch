49ce9685818b3321f857f229ecd449067e0148e6
johnpyp <johnpaulpenaloza@gmail.com>
2019-06-11 10:32:34 -0400

    Readded cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import {logger} from '@lib/logger'
 import helmet from 'helmet'
+import cors from 'cors'
 import api from '@routes/api'
 const prefix = process.env.ROUTE_PREFIX
 
@@ -10,6 +11,7 @@ app.use((req, res, next) => {
   logger.info(req)
   next()
 })
+app.use(cors())
 app.use(helmet())
 app.use(prefix, api)
 app.use(function(err, req, res, next) {
