023d5c1fa2d533d55715ed3cf31ee6f46a143542
alg1142 <48770555+alg1142@users.noreply.github.com>
2019-06-11 04:05:40 +0300

    improve req logging

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,13 +1,49 @@
 import pino from 'pino'
 import {isProd} from '@lib/environment'
+import {DateTime} from 'luxon'
 
 const name = process.env.APP_NAME
+
+const getDate = timestamp => {
+  const parsed = parseInt(timestamp)
+  if (isNaN(parsed)) return ''
+  const date = DateTime.fromMillis(parsed)
+  return date.toFormat('yyyy-MM-dd')
+}
+
+const getLoggerInfo = req => {
+  const ip = req.headers['X-Real-IP']
+  const url = `https://meme.com${req.url}`
+  const params = new URL(url).searchParams
+
+  const channel = params.get('channel') ?? ''
+  const username = params.get('username') ?? ''
+  const text = params.get('text') ?? ''
+  const startingDate = params.get('startingDate') ?? ''
+  const endingDate = params.get('endingDate') ?? ''
+
+  return {
+    ip,
+    channel,
+    username,
+    text,
+    startingDate: getDate(startingDate),
+    endingDate: getDate(endingDate),
+  }
+}
 
 const pinoOptions = {
   name,
   base: {name},
   level: process.env.LOG_LEVEL,
   prettyPrint: !isProd(),
+  serializers: {
+    req(req) {
+      return {
+        ...getLoggerInfo(req),
+      }
+    },
+  },
 }
 
 export const logger = pino(pinoOptions)
