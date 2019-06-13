10615536e3347bc4920975a6064a41fa2932dbc9
alg1142 <48770555+alg1142@users.noreply.github.com>
2019-06-13 18:03:10 +0300

    add test for ratelimit

--- api/__tests__/server.test.js
+++ api/__tests__/server.test.js
@@ -8,6 +8,17 @@ const getApiURL = path =>
   `${process.env.ROUTE_PREFIX}/${path}`
 >>>>>>> move src to api:api/__tests__/server.test.js
 
+const getManyRequests = (n, url, query) => {
+  const requests = []
+  for (let i = 0; i < n; i += 1) {
+    const req = request(app)
+      .get(getApiURL(url))
+      .query(query)
+    requests.push(req)
+  }
+  return requests
+}
+
 describe('server test', () => {
   test('healthcheck passes', async() => {
     const response = await request(app).get(getApiURL('healthcheck'))
@@ -21,5 +32,14 @@ describe('server test', () => {
       .query({channel: 'destinygg'})
 
     expect(response.statusCode).toBe(200)
+  })
+
+  test('the ratelimiter works', async done => {
+    const requests = getManyRequests(20, 'search', {
+      channel: 'destinygg',
+    })
+    // at least one should throw a 429
+    for await (const response of requests)
+      if (response.statusCode === 429) done()
   })
 })
