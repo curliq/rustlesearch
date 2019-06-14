83b997c6a4e34fff56314e44c36aa28d0f1b6b2f
alg1142 <48770555+alg1142@users.noreply.github.com>
2019-06-14 03:05:47 +0300

    remove util fns

--- server/api/lib/util.js
+++ server/api/lib/util.js
@@ -2,6 +2,3 @@ import Promise from 'bluebird'
 
 export const co = fn => Promise.coroutine(fn)
 export const fs = Promise.promisifyAll(require('fs'))
-export const pad = n => `00${n}`.slice(-2)
-export const getISODayDate = date =>
-  `${date.getFullYear()}-${pad(date.getMonth())}-${pad(date.getDay())}`
