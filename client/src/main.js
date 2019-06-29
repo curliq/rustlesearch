8c1a5bf18fb61984ddd1afe8c0e65510ff4cc658
johnpyp <johnpaulpenaloza@gmail.com>
2019-06-29 01:55:54 -0400

    fixed minor issue

--- client/src/main.js
+++ client/src/main.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import VueMask from 'v-mask'
 import Notifications from 'vue-notification'
 import './global-base-components.js'
 
 import './main.css'
 
-Vue.use(VueMask)
 Vue.use(Notifications)
 Vue.config.productionTip = false
 
