ca49d13118e7c276d5824f3829230781a6f1d07c
johnpyp <johnpaulpenaloza@gmail.com>
2019-06-29 13:07:37 -0400

    Changed to fontawesome with async loading

--- client/src/components/ToggleModeBar.vue
+++ client/src/components/ToggleModeBar.vue
@@ -18,11 +18,11 @@
     </div>
     <div class="border-l self-stretch border-gray-400" />
     <span
-      :class="[sharedClasses, 'mdi-view-stream', !isCompact ? activeClass : nonActiveClass]"
+      :class="[sharedClasses, 'fa-equals', !isCompact ? activeClass : nonActiveClass]"
       @click="$emit('update:mode', 'message')"
     />
     <span
-      :class="[sharedClasses, 'mdi-view-headline', isCompact ? activeClass : nonActiveClass]"
+      :class="[sharedClasses, 'fa-bars', isCompact ? activeClass : nonActiveClass]"
       @click="$emit('update:mode', 'compact-message')"
     />
   </div>
@@ -43,7 +43,7 @@ export default {
   data () {
     return {
       sharedWordClass: 'px-3 self-stretch font-semibold flex items-center',
-      sharedClasses: 'text-2xl mdi px-3',
+      sharedClasses: 'self-stretch flex items-center text-2xl fas px-3',
       activeClass: 'bg-gray-600 cursor-default',
       nonActiveClass: 'hover:bg-gray-700 cursor-pointer'
     }
