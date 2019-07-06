63d43a3d302c064a18df1bd18e0ac81777c0e977
johnpyp <johnpaulpenaloza@gmail.com>
2019-07-05 22:26:30 -0400

    Improved benchmark script

--- scripts/src/benchmark-ingest.js
+++ scripts/src/benchmark-ingest.js
@@ -1,5 +1,5 @@
 const Promise = require('bluebird')
-const {range} = require('ramda')
+const {range, append} = require('ramda')
 const {Client} = require('@elastic/elasticsearch')
 const {co} = require('../util')
 
@@ -8,6 +8,7 @@ const client = new Client({
 })
 
 const averageDelta = timeseries => {
+  if (timeseries.length < 2) return 0
   const changes = timeseries.length - 1
   const [start] = timeseries
   const end = timeseries[changes]
@@ -15,29 +16,29 @@ const averageDelta = timeseries => {
   return parseInt((end - start) / changes)
 }
 
-const getCount = co(function* getCount(timeout) {
-  yield Promise.delay(timeout * 1000 * 60)
-
+const getCount = co(function* getCount(total, timeout) {
   const result = yield client.count({
     index: process.env.INDEX_NAME,
   })
 
+  const newTotal = append(result.body.count, total)
+
   console.info({
-    currentMessages: result.body.count,
-    elapsedTime: `${timeout}m`,
-    formatted: result.body.count.toLocaleString('en-US'),
+    avg: averageDelta(newTotal).toLocaleString('en-US'),
+    count: result.body.count.toLocaleString('en-US'),
+    time: `${timeout}m`,
   })
 
-  return result.body.count
+  yield Promise.delay(1000 * 60)
+
+  return newTotal
 })
 
 const main = () => {
   console.log(process.argv[2])
+  const minutesRange = range(0, parseInt(process.argv[2]) || 10)
 
-  return Promise.map(
-    range(0, process.argv[2] ? parseInt(process.argv[2]) : 10),
-    getCount,
-  )
+  return Promise.reduce(minutesRange, getCount, [])
 
     .then(averageDelta)
     .then(speed =>
