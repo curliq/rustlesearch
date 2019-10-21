f8f032abd0190f66ac316707d1674573f3d1e461
johnpyp <johnpaulpenaloza@gmail.com>
2019-10-21 11:18:45 -0400

    Refined about page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -4,13 +4,26 @@
       <div
         class="bg-gray-850 text-white shadow flex flex-col rounded py-4 px-3"
       >
-        <h1>Servers are expensive. please send help</h1>
-        <a
-          href="https://patreon.com/rustlesearch"
-          class="text-accent hover:underline"
-          target="_blank"
-          >patreon</a
-        >
+        <h1 class="text-lg font-bold mb-4">About Rustlesearch</h1>
+        <p class="mb-2">
+          Rustlesearch is a web application to search an index of billions of
+          twitch messages. View a more comprehensive description on our
+          <a
+            href="https://github.com/johnpyp/rustlesearch"
+            class="text-accent hover:underline"
+            target="_blank"
+            >github</a
+          >.
+        </p>
+        <p>
+          You can support us on
+          <a
+            href="https://patreon.com/rustlesearch"
+            class="text-accent hover:underline"
+            target="_blank"
+            >patreon</a
+          >.
+        </p>
       </div>
     </div>
   </div>
