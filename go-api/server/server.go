bfb6329e10572ccb299ab9e2cbdf6d6537a78c82
johnpyp <johnpaulpenaloza@gmail.com>
2019-11-03 21:24:28 -0500

    Added docker and docker-compose to go-api

--- go-api/server/server.go
+++ go-api/server/server.go
@@ -1,9 +1,12 @@
 package server
 
-import "github.com/johnpyp/rustlesearch/go-api/config"
+import (
+	"fmt"
+	"github.com/johnpyp/rustlesearch/go-api/config"
+)
 
 func Init() {
 	config := config.GetConfig()
 	r := NewRouter()
-	r.Run(config.GetString("server.port"))
+	r.Run(fmt.Sprintf(":%s", config.GetString("server.port")))
 }
