05065035042c28ef4b7f7eb1e0d6959f8e132ff1
mmacheerpuppy <callum.leach@hotmail.co.uk>
2019-11-04 16:20:19 +0000

    + Remove unused structs

--- go-api/rustlesearch/models.go
+++ go-api/rustlesearch/models.go
@@ -27,11 +27,4 @@ type SearchQuery struct {
 	SearchAfter int       `json:"searchAfter" form:"search_after"`
 }
 
-type Search struct {
-	StartDate   time.Time `json:"startDate" form:"start_date" time_format:"2006-01-02"`
-	EndDate     time.Time `json:"endDate" form:"end_date" time_format:"2006-01-02"`
-	Channel     string    `json:"channel" form:"channel"`
-	Username    string    `json:"username" form:"username"`
-	Text        string    `json:"text" form:"text"`
-	SearchAfter int       `json:"searchAfter" form:"search_after"`
-}
+type Search struct {}
