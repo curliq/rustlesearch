5790c7316c812f2647ba5305fef3e8de4666061a
johnpyp <johnpaulpenaloza@gmail.com>
2019-11-04 14:40:10 -0500

    Removed unnecessary fields in Search struct

--- go-api/rustlesearch/models.go
+++ go-api/rustlesearch/models.go
@@ -11,6 +11,7 @@ type Message struct {
 }
 
 type Surrounds struct{}
+type Search struct{}
 
 type SurroundsQuery struct {
 	Channel     string `json:"channel" form:"channel" binding:"required"`
@@ -19,15 +20,6 @@ type SurroundsQuery struct {
 }
 
 type SearchQuery struct {
-	StartDate   time.Time `json:"startDate" form:"start_date" time_format:"2006-01-02"`
-	EndDate     time.Time `json:"endDate" form:"end_date" time_format:"2006-01-02"`
-	Channel     string    `json:"channel" form:"channel"`
-	Username    string    `json:"username" form:"username"`
-	Text        string    `json:"text" form:"text"`
-	SearchAfter int       `json:"searchAfter" form:"search_after"`
-}
-
-type Search struct {
 	StartDate   time.Time `json:"startDate" form:"start_date" time_format:"2006-01-02"`
 	EndDate     time.Time `json:"endDate" form:"end_date" time_format:"2006-01-02"`
 	Channel     string    `json:"channel" form:"channel"`
