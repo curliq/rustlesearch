6b4e862f2ef56f964cbfc7b705e5d961f68acf9a
johnpyp <johnpaulpenaloza@gmail.com>
2019-11-04 22:16:56 -0500

    Deleted node server

--- server/jest.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  setupFiles: ["dotenv/config"],
-  testEnvironment: "node",
-};
--- server/src/middleware/request-extender.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req, res, next) => {
-  req.realIp = req.headers["X-Real-IP"];
-  next();
-};
