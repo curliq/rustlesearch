4fda53f5bb80bd273aef2bd7231bdbacb67c9df2
johnpyp <johnpaulpenaloza@gmail.com>
2020-03-04 10:45:22 -0500

    Added initial scraper setup

---
+++ scraper/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  rules: {
+    "no-console": 0,
+  },
+};
---
+++ scraper/src/config.js
@@ -0,0 +1,8 @@
+const confic = require("confic");
+
+module.exports = confic(
+  {
+    port: 3000,
+  },
+  { inspect: true },
+);
