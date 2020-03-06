15d2e5de0ee672f17c9af6ff5fc9e6fa3fdcc661
johnpyp <johnpaulpenaloza@gmail.com>
2020-03-06 10:11:29 -0500

    Added basic chat logging

--- scraper/src/index.js
+++ scraper/src/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 const config = require("./config");
+const clientFactory = require("./client-factory");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const clientBuilder = clientFactory({});
+
+const client = clientBuilder(["destiny"]);
+app.get("/check-channels", (req, res) => res.send("Hello World!"));
 
 app.listen(config.port, () =>
   console.log(`Example app listening on port ${config.port}!`),
