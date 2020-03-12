9038e0a243cf937dc8d1f3e4dd7ec593b988e9c8
johnpyp <johnpaulpenaloza@gmail.com>
2020-03-12 14:50:13 -0400

    Replaced website with temp notice

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,14 @@
 <template>
+  <!--
   <div id="app" class="bg-gray-950 w-full min-h-screen m-0">
     <base-notifications />
     <navbar />
     <div class="mt-8 lg:px-8 md:px-4 px-2">
       <router-view />
     </div>
+  </div> -->
+  <div>
+    Rustlesearch is undering major changes, come back 03/15
   </div>
 </template>
 <script>
