6dba8c06fddbf1ac75e9a61df4b123b0362dfc06
johnpyp <johnpaulpenaloza@gmail.com>
2020-04-19 10:30:33 -0400

    Fixed recompress

--- recompress.py
+++ recompress.py
@@ -15,10 +15,13 @@ def process_fname(fname):
     with open(fname, "rb") as content:
         b = content.read()
     s = zlib.decompress(b)
-    with gzip.open(fname[:-3] + ".gz", "wb") as f_out:
+    gzip_name = fname[:-3] + ".gz"
+    gzip_tmp_name = fname[:-3] + ".gz.tmp"
+    with gzip.open(gzip_tmp_name, "wb") as f_out:
         f_out.write(s)
 
-    # os.remove(fname)
+    os.rename(gzip_tmp_name, gzip_name)
+    os.rename(fname, "./orl_bak/" + os.path.basename(fname))
     global counter
     with counter.get_lock():
         counter.value += 1
@@ -26,9 +29,14 @@ def process_fname(fname):
 
 
 if __name__ == "__main__":
+    os.makedirs("orl_bak", exist_ok=True)
+    tmps = glob.glob("./orl/*.tmp")
+    print(f"Removing {len(tmps)} tmp files...")
+    for fname in tmps:
+        os.remove(fname)
     fnames = glob.glob("./orl/*.zz")
-    print(len(fnames))
+    print(f"Found {len(fnames)} files to recompress...")
     print("Starting recompression...")
     time.sleep(1)
-    p = Pool(5)
+    p = Pool(10)
     p.map(process_fname, fnames)
