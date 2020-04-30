6a749c8801086606ffc0ae33c29d3043c11fada8
johnpyp <johnpaulpenaloza@gmail.com>
2020-04-30 08:50:09 -0400

    Fixed dgg scraper

--- scraper/src/scrapers/dgg-scraper.js
+++ scraper/src/scrapers/dgg-scraper.js
@@ -1,5 +1,6 @@
 const ReconnectingWebSocket = require("reconnecting-websocket");
 const WebSocket = require("ws");
+const R = require("ramda");
 
 class DggScraper {
   constructor({ dggScraper }, writers) {
@@ -23,22 +24,107 @@ class DggScraper {
       const split = data.indexOf(" ");
       const type = data.slice(0, split);
       const msg = JSON.parse(data.slice(split + 1));
-      const transformations = {
-        MSG: {
-          channel: "destinygg",
-          username: msg.nick,
-          text: msg.data,
-          ts: msg.timestamp,
-        },
-        BROADCAST: {
-          channel: "destinygg",
-          username: "subscriber",
-          text: msg.data,
-          ts: msg.timestamp,
-        },
-      };
-      if (type in transformations) {
-        this.sendToWriters(transformations[type]);
+
+      const giftRegex = /^[a-zA-Z0-9_]+ gifted [a-zA-Z0-9_]+ a Tier (I|II|II|IV) subscription!/;
+      const toSend = R.cond([
+        [
+          R.equals("BAN"),
+          () => ({
+            channel: "destinygg",
+            username: "Ban",
+            text: `${msg.data} banned by ${msg.nick}`,
+            ts: msg.timestamp,
+          }),
+        ],
+        [
+          R.equals("UNBAN"),
+          () => ({
+            channel: "destinygg",
+            username: "Ban",
+            text: `${msg.data} unbanned by ${msg.nick}`,
+            ts: msg.timestamp,
+          }),
+        ],
+        [
+          R.equals("MUTE"),
+          () => ({
+            channel: "destinygg",
+            username: "Ban",
+            text: `${msg.data} muted by ${msg.nick}`,
+            ts: msg.timestamp,
+          }),
+        ],
+        [
+          R.equals("UNMUTE"),
+          () => ({
+            channel: "destinygg",
+            username: "Ban",
+            text: `${msg.data} unmuted by ${msg.nick}`,
+            ts: msg.timestamp,
+          }),
+        ],
+        [
+          R.equals("MSG"),
+          () => ({
+            channel: "destinygg",
+            username: msg.nick,
+            text: msg.data,
+            ts: msg.timestamp,
+          }),
+        ],
+        [
+          R.equals("BROADCAST"),
+          () => {
+            const subMessages = [
+              "subscriber!",
+              "subscribed on Twitch!",
+              "has resubscribed! Active for",
+              "has resubscribed on Twitch! active",
+            ];
+            for (const subMsg of subMessages) {
+              if (msg.data.includes(subMsg)) {
+                return {
+                  channel: "destinygg",
+                  username: "subscriber",
+                  text: msg.data,
+                  ts: msg.timestamp,
+                };
+              }
+            }
+            if (giftRegex.test(msg.data)) {
+              return {
+                channel: "destinygg",
+                username: "subscriber",
+                text: msg.data,
+                ts: msg.timestamp,
+              };
+            }
+            if (msg.data.includes("said...")) {
+              return {
+                channel: "destinygg",
+                username: "subscribermessage",
+                text: msg.data,
+                ts: msg.timestamp,
+              };
+            }
+            return {
+              channel: "destinygg",
+              username: "broadcast",
+              text: msg.data,
+              ts: msg.timestamp,
+            };
+          },
+        ],
+        [
+          R.T,
+          () => {
+            // console.log("Uncaught type", type, msg);
+            return null;
+          },
+        ],
+      ])(type);
+      if (toSend) {
+        this.sendToWriters(toSend);
       }
     });
   }
