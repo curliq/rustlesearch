0358652ed60da4aa0d00c5e7bde583d44d4a44e1
johnpyp <johnpaulpenaloza@gmail.com>
2020-05-01 23:50:03 -0400

    Added new file format to other parts

---
+++ scraper/src/orl-fs.js
@@ -0,0 +1,82 @@
+const fg = require("fast-glob");
+const { join, parse, sep } = require("path");
+const _ = require("lodash");
+const zlib = require("zlib");
+const { promisify } = require("util");
+const { fs } = require("./util");
+
+const gunzip = promisify(zlib.gunzip);
+
+module.exports = ({ orl, channelsPath }) => {
+  return {
+    fetchChannels() {
+      return fs.readFile(channelsPath, "utf8").then((text) => new Set(text.trim().split("\n")));
+    },
+    async listLogs(channel) {
+      await fs.ensureDir(orl);
+      const paths = channel ? await fg(`${orl}/${channel}/*.txt*`) : await fg(`${orl}/*/*.txt*`);
+      return paths.map((p) => {
+        const logChannel = _.last(parse(p).dir.split(sep));
+
+        const filename = parse(p).base;
+        if (filename.endsWith(".txt.gz")) {
+          const date = filename.replace(".txt.gz", "");
+          return {
+            channel: logChannel,
+            date,
+            path: p,
+            combo: `${logChannel}/${date}`,
+            compressed: true,
+          };
+        }
+        if (filename.endsWith(".txt")) {
+          const date = filename.replace(".txt", "");
+          return {
+            channel: logChannel,
+            date,
+            path: p,
+            combo: `${logChannel}/${date}`,
+            compressed: false,
+          };
+        }
+        throw new Error("Weird file");
+      });
+    },
+    listCompressedLogs(channel) {
+      return this.listLogs(channel).then((logs) => logs.filter((log) => log.compressed));
+    },
+    listUncompressedLogs(channel) {
+      return this.listLogs(channel).then((logs) => logs.filter((log) => !log.compressed));
+    },
+    getPath({ channel, date, compressed = true }) {
+      if (compressed === true) {
+        return join(orl, channel, `${date}.txt.gz`);
+      }
+      return join(orl, channel, `${date}.txt`);
+    },
+    buildComboString({ channel, date }) {
+      return `${channel}/${date}`;
+    },
+    async readLog({ channel, date, compressed }) {
+      const path = this.getPath({ channel, date, compressed });
+      if (!(await fs.pathExists(path))) {
+        return "";
+      }
+      if (compressed === true) {
+        const raw = await fs.readFile(path);
+        const buffer = await gunzip(raw);
+        return buffer.toString();
+      }
+      return fs.readFile(path, "utf8");
+    },
+    async readLogUnsafe({ channel, date, compressed }) {
+      const path = this.getPath({ channel, date, compressed });
+      if (compressed === true) {
+        const raw = await fs.readFile(path);
+        const buffer = await gunzip(raw);
+        return buffer.toString();
+      }
+      return fs.readFile(path, "utf8");
+    },
+  };
+};
--- scraper/src/writers/file-writer.js
+++ scraper/src/writers/file-writer.js
@@ -1,14 +1,19 @@
 const fs = require("fs-extra");
-const { join } = require("path");
+const NodeCache = require("node-cache");
 
 const TransformService = require("../transform-service");
+const buildOrlFs = require("../orl-fs");
 
 class FileWriter {
   constructor(cfg) {
     this.dir = cfg.paths.orl;
 
     this.cfg = cfg;
-    this.streamMap = new Map();
+    this.streamMap = new NodeCache({ stdTTL: 3600, useClones: false });
+    this.orlFs = buildOrlFs({
+      orl: cfg.paths.orl,
+      channelsPath: cfg.paths.channels,
+    });
   }
 
   async setup() {
@@ -16,19 +21,21 @@ class FileWriter {
     await fs.ensureDir(this.dir);
   }
 
-  getFileWriteStream(channel, day) {
-    const filepath = join(this.dir, `${channel}::${day.format("YYYY-MM-DD")}.txt`);
-    if (this.streamMap.has(filepath)) {
-      return this.streamMap.get(filepath);
+  async getFileWriteStream(channel, day) {
+    const path = this.orlFs.getPath({ channel, date: day.format("YYYY-MM-DD"), compressed: false });
+    if (this.streamMap.has(path)) {
+      return this.streamMap.get(path);
     }
-    const writeStream = fs.createWriteStream(filepath, { flags: "a", encoding: "utf8" });
-    this.streamMap.set(filepath, writeStream);
-    return this.streamMap.get(filepath);
+    await fs.ensureFile(path);
+    const writeStream = fs.createWriteStream(path, { flags: "a", encoding: "utf8" });
+    this.streamMap.set(path, writeStream);
+    return this.streamMap.get(path);
   }
 
-  write({ channel, text, ts, username }) {
+  async write({ channel, text, ts, username }) {
     const msg = TransformService.toMessage({ channel, text, ts, username });
-    this.getFileWriteStream(msg.channel, msg.ts).write(`${TransformService.msgToLine(msg)}\n`);
+    const stream = await this.getFileWriteStream(msg.channel, msg.ts);
+    stream.write(`${TransformService.msgToLine(msg)}\n`);
   }
 }
 
